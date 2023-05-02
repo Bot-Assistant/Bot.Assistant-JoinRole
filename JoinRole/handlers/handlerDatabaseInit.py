a09f6ffab045a6a0e74f4ea311fce3daf028560a
Ted <84779771+Ted-18@users.noreply.github.com>
2023-05-02 17:46:04 +0200

    1.1.6

--- JoinRole/handlers/handlerDatabaseInit.py
+++ JoinRole/handlers/handlerDatabaseInit.py
@@ -1,12 +1,24 @@
 import services.serviceDatabase as serviceDatabase
+import settings.settingBot as settingBot
 
 # Create the database if it does not exist
 def databaseInit():
-    # Table structure
-    tableName = "addon_joinrole_roles"
-    columns = [
-        ["serverID", "BIGINT NOT NULL"], 
-        ["roleID", "BIGINT NOT NULL"]
-    ]
-    serviceDatabase.databaseCreation(tableName, columns)
+    if settingBot.databaseType == "MariaDB":
+        # Table structure
+        tableName = "addon_joinrole_roles"
+        columns = [
+            ["serverID", "BIGINT NOT NULL"], 
+            ["roleID", "BIGINT NOT NULL"]
+        ]
+        serviceDatabase.databaseCreation(tableName, columns)
+
+
+    elif settingBot.databaseType == "SQLite":
+        # Table structure
+        tableName = "addon_joinrole_roles"
+        columns = [
+            ["serverID", "integer NOT NULL"], 
+            ["roleID", "integer NOT NULL"]
+        ]
+        serviceDatabase.databaseCreation(tableName, columns)
 
